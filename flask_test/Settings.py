e6c6eeeb8ff678c815fcbc60354df48862ca71f4
Stanko <stanko@bezak.net>
2021-04-16 13:53:50 +0200

    database fix

--- flask_test/Settings.py
+++ flask_test/Settings.py
@@ -1,12 +1,9 @@
-import os 
+import os
+import re
 
-uri = os.environ.get('DATABASE_URL')
-
-
-if 'sqlite' not in uri:
-    # musime zmenit uri databazy, nakolko to defaultne nespolupracuje (v novej verzii sqlalchemy bolo zrusene)
-    # a na heroku sa to zmenit neda
-    uri.replace("://", "ql://", 1)
+uri = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 
 SQLALCHEMY_DATABASE_URI = uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
